030bad2ea14ab21799797a1866aa50dcc7aae3de
Riham <riham994@gmail.com>
2026-05-31 17:52:07 +0200

    add comment

---
+++ src/main/java/day2/Operators.java
@@ -0,0 +1,134 @@
+package day2;
+
+public class Operators {
+    static void main(String[] args) {
+
+       // int x=10;
+      //  int y=20;
+      //  System.out.println(x==y); //false
+      //  System.out.println(x=y); //x=20
+     //   System.out.println("x ="+ x);
+       // System.out.println(x==y); //true
+
+
+     //   double a=15.0;
+       // int b=15;
+        //System.out.println(a==b); //true
+       // System.out.println(a!=b); //false
+
+
+//        int a= 25;
+//        System.out.println("The value of a = "+a); //25
+//        int b = 25 ;
+//        System.out.println("The Sum of a and b = "+(a+b)); //50
+//        a = a+5; //30
+//        a+=5; //shorthand assignment ->     a= a+5 =35
+//        System.out.println(a);
+//        a = a-5; //30
+//        a-=5; //shorthand assignment ->     a= a+5 =25
+//        System.out.println("The sub of a-5 = "+a);
+//            a=a*5;
+//            a*=5;
+//        System.out.println("The Multi of a*5 = "+a);
+//         a= a/5;
+//         a/=5;
+//        System.out.println("The div of a/5 = "+a);
+
+
+
+//        int x =20;
+//        int y=3;
+//        System.out.println(x%y);
+
+  //Relational operators   -> Binary
+//        int a = 7;
+//        int b = 5;
+//        int c=7;
+//        int d =7;
+//        System.out.println(a > b); //T
+//        System.out.println(a < b); //F
+//        System.out.println(a == b);//F
+//        System.out.println(a >= b); //T
+//        System.out.println(a <= b); //F
+
+// Logical Operators -> Unary and Binary
+//        boolean v1= true;
+//        boolean v2 = false;
+//        System.out.println(v1 && v2); // AND => F
+//        System.out.println(v1 || v2); // oR =>T
+//
+//        System. out.println(a > b && b < c); // true
+
+//          //                  T          T
+//        System.out.println(a > b && b > c); // false
+//
+//        System.out.println ( a < b ||  d >a ); // false
+//                           F         F
+//
+//        System.out.println(a<= b || d >= a);
+//
+//int  adbi =20;
+//int almi =15;
+//boolean tamioz = true  ;
+//if (adbi >= almi || tamioz)
+//    System.out.println("almi" ); //t
+//else
+//    System.out.println("adbi" ); //f
+//
+//// Increment and Decrement Operators
+//        int num = 10;
+//        System.out.println(num++); // post increment => num + 1 (Save in memory)
+//        System.out.println(num); // 11
+//        System.out.println(++num); //12 pre increment => increase (num) then display
+//        int number = 5;
+//        System.out.println(number++); //5
+//        System.out.println(number); //6
+//        System.out.println(++number); //7
+// ternary operator
+//        int a =18, b=10 , c;
+//        c=(a>b)? a:b; //if a>b is true then c= a if False c=b
+//        System.out.println("The largest value is: "+c);
+//        if (a>b)
+//            c=a;
+//        else
+//            c=b ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
